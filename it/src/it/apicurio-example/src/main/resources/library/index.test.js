85e26f28f464d4d44914151fd036e3a405ae8f7e
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2025-05-08 13:31:09 +0100

    minor

--- it/src/it/apicurio-example/src/main/resources/library/index.test.js
+++ it/src/it/apicurio-example/src/main/resources/library/index.test.js
@@ -4,10 +4,10 @@ import { validate } from "./index.js";
 import { log } from "./apicurio.mjs";
 
 vi.mock("./apicurio.mjs", () => ({
-  log: vi.fn().mockImplementation((str) => console.log(`Mocked log: {str}`))
+  log: vi.fn().mockImplementation((str) => console.log(`Mocked log: ${str}`))
 }));
 
 test('can validate artifacts', () => {
-    expect(validate({ name: "xyz", type: "openapi"})).toBe(true);
-    expect(validate({ name: "xyz", type: "raml"})).toBe(false);
+    expect(validate({ name: "my-openapi", type: "openapi"})).toBe(true);
+    expect(validate({ name: "my-raml", type: "raml"})).toBe(false);
 });
